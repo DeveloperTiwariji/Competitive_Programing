88f16e215c283f2b1f04656d5e3b9ff4f874d7d8
Satyam Tiwari <satyam.12207441@lpu.in>
2024-10-06 20:04:05 +0530

    check

--- LargestOddNumInString.java
+++ LargestOddNumInString.java
@@ -5,6 +5,7 @@ public class LargestOddNumInString{
     public static void main(String[] args){
         System.out.println(largestOddNumber("35427"));
     }
+    
      public static String largestOddNumber(String num) {
       int l = num.length();
       for(int i=l-1;i>=0;i--){
